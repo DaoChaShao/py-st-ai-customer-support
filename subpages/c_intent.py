9e859126b093623d1c45cef4b884fc1d9145d91f
DaoChaShao <27882272@qq.com>
2025-07-09 14:41:02 +0800

    feat: add c_intent.py with main function template

---
+++ subpages/c_intent.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/7/9 14:29
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   c_intent.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
