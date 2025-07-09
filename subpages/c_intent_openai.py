92afd1bbbe88d68662ba35aeee8b5868cd52c754
DaoChaShao <27882272@qq.com>
2025-07-10 00:23:51 +0800

    fix: rename c_intent.py to c_intent_openai.py for clarity

--- subpages/c_intent_openai.py
+++ subpages/c_intent_openai.py
@@ -3,7 +3,7 @@
 # @Time     :   2025/7/9 14:29
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   c_intent.py
+# @File     :   c_intent_openai.py
 # @Desc     :   
 
 def main() -> None:
